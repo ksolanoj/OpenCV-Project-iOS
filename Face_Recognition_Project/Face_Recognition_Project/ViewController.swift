4902d558a8ad01a4caac7b8454029463df94ea57
Kevin Solano Jiménez <ksolanoj@icloud.com>
2020-10-14 02:01:21 -0600

    Initial Project

---
+++ Face_Recognition_Project/Face_Recognition_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Face_Recognition_Project
+//
+//  Created by Kevin Solano Jimenez on 10/14/20.
+//  Copyright © 2020 Kevin Solano Jimenez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
